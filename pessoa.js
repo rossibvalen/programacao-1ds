83db7430711ea7f5209bebd7cc27a7aec35328b7
rossibvalen <rossi.valentina@escola.pr.gov.br>
2025-11-26 14:57:44 -0300

    Create pessoa.js

---
+++ pessoa.js
@@ -0,0 +1,10 @@
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  apresentar() {
+    console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+  }
+}
